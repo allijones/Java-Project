20d2c962ad727d8ac159fe3249039a04b7c9625a
Alli Jones <Alli_Jones1@baylor.edu>
2018-03-31 12:58:51 -0500

    Database DAYS has been created

--- src/main/java/CreateTable.java
+++ src/main/java/CreateTable.java
@@ -7,22 +7,35 @@ import java.sql.Statement;
 public class CreateTable {
 
     private static final String DB_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-    private static final String DB_CONNECTION = "jdbc:derby:table1;create=true";
+    private static final String DB_CONNECTION = "jdbc:derby:database;create=true";
     private static final String DB_USER = "";
     private static final String DB_PASSWORD = "";
 
     public static void createTable() throws SQLException{
         Connection dbConnection = null;
         Statement statement = null;
+        Statement statement1 = null;
         String createString = "CREATE TABLE EMPLOYEES(" +
                 "ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
                 "NAME VARCHAR(20) NOT NULL " + ")";
-        Statement stmt = null;
+        String createString2 = "CREATE TABLE DAYS(" +
+                "ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
+                "MONDAY BOOLEAN, " +
+                "TUESDAY BOOLEAN, " +
+                "WEDNESDAY BOOLEAN, " +
+                "THURSDAY BOOLEAN, " +
+                "FRIDAY BOOLEAN, " +
+                "SATRUDAY BOOLEAN, " +
+                "SUNDAY BOOLEAN " + ")";
+        //Statement stmt = null;
         try {
             dbConnection = getDBConnection();
             statement = dbConnection.createStatement();
+            statement1 = dbConnection.createStatement();
             statement.execute(createString);
+            statement1.execute(createString2);
             System.out.print("Table \"Employees\" is created!");
+            System.out.print("Table \"Days\" is created!");
         }
         catch(SQLException e){
             System.out.println(e.getMessage());
