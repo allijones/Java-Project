1fabb246c5210eda82e1d988438984bb40fc61a2
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2018-04-22 14:32:03 -0500

    Added ability to creat Employee with ID

--- src/main/java/EmployeeFactory.java
+++ src/main/java/EmployeeFactory.java
@@ -7,4 +7,13 @@ public class EmployeeFactory {
         }
         return out;
     }
+    public static Employee create(String name, boolean[] schedule, int id){
+        Employee out = new Employee();
+        out.setName(name);
+        for(int i = 0; i < 7; i++){
+            out.setAvalability(Day.fromIndex(i), schedule[i]);
+        }
+        out.setId(id);
+        return out;
+    }
 }
