f86f4886f976b623fdad5cffbffd035a93a4a8b8
Unknown <Aidan.S.Edwards@gmail.com>
2018-04-23 15:43:13 -0500

    Added some more tests

--- test/main/java/TestActionMenu.java
+++ test/main/java/TestActionMenu.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -6,10 +7,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TestActionMenu {
-    @Test
-    void testOne(){
-        List<Integer> order = new ArrayList<>();
-        ActionMenu testMenu = new ActionMenu();
+    private static List<Integer> order;
+    private static ActionMenu testMenu;
+    @BeforeAll
+    static void setUp(){
+        order = new ArrayList<>();
+        testMenu = new ActionMenu();
         testMenu.add("Action 1", (ActionMenu)->{
             order.add(1);
         });
@@ -23,8 +26,30 @@ public class TestActionMenu {
             order.add(4);
         });
         testMenu.add("Cancel Action", ActionMenu::stop);
+
+    }
+    @Test
+    void testOne(){
         testMenu.run(new Scanner("4 2 3 1 4 4 5"), new NullPrintStream());
         int[] verify = {4, 2, 3, 1, 4, 4};
         Assertions.assertArrayEquals(order.stream().mapToInt(i->i).toArray(), verify);
+        order.clear();
+    }
+    @Test
+    void testTwo(){
+        testMenu.run(new Scanner("1 2 3 4 3 2 1 2 3 2 1 1 1 2 3 4 3 4 3 3 3 5"), new NullPrintStream());
+        int[] verify = {1, 2, 3, 4, 3, 2, 1, 2, 3, 2, 1, 1, 1, 2, 3, 4, 3, 4, 3, 3, 3};
+        Assertions.assertArrayEquals(order.stream().mapToInt(i->i).toArray(), verify);
+        order.clear();
+    }
+    @Test
+    void testThree(){
+        testMenu.add("Action 6", (ActionMenu)->{
+            order.add(6);
+        });
+        testMenu.run(new Scanner("1 2 6 3 4 1 2 6 3 2 4 6 1 2 3 6 1 2 3 4 5"), new NullPrintStream());
+        int[] verify = {1, 2, 6, 3, 4, 1, 2, 6, 3, 2, 4, 6, 1, 2, 3, 6, 1, 2, 3, 4};
+        Assertions.assertArrayEquals(order.stream().mapToInt(i->i).toArray(), verify);
+        order.clear();
     }
 }
